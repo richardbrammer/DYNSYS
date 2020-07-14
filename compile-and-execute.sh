469745513d7388100c10f61e6701c7e8c0b9c681
Richard Brammer <office@richardbrammer.de>
2020-07-14 15:35:03 +0200

    CHORE rename model files

--- compile-and-execute.sh
+++ compile-and-execute.sh
@@ -5,5 +5,5 @@ if [ ! -d build ]; then mkdir -p build; fi
 gcc src/Main.c src/Lexer.c -o build/Dynsys
 ln -sf build/Dynsys Dynsys
 chmod +x Dynsys
-echo 'e.g. use "examples/adoption_1.dynsys" as file'
+echo 'e.g. use "examples/adoption.dynsys" as file'
 ./Dynsys
